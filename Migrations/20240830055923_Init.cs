2f64d62bf0f945f1e0b29b1280f61a25b06e2340
vvsanga <vvsanga@gmail.com>
2024-08-30 14:26:10 +0800

    Add Migration

---
+++ Migrations/20240830055923_Init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VVCRUD_IT_CDN_API_NET8.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Professional",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    Skillset = table.Column<string>(type: "nvarchar(500)", nullable: false),
+                    Hobby = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CreateDateTime = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professional", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Professional");
+        }
+    }
+}
